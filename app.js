255f6c6c529f40b0c285dfda14099f1b800b5d6c
jousef1 <yousefel560@gmail.com>
2023-08-06 23:19:40 +0800

    user modle

--- app.js
+++ app.js
@@ -2,28 +2,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
+const logger = require('./middlewares/logger');
+const {notFound,errorHanlder} = require('./middlewares/erorrs');
+
 
 
 mongoose
-.connect("mongodb://localhost/bookStoreDB")
+.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected To MongoDB..."))
 .catch((error) => console.log("Connection Failed To MongoDB!", error));
-
 
 
 
 const bookspath = require('./routes/books');
 const authorspath = require('./routes/authors');
 
+
+//Routes
 app.use(express.json())
-const Joi = require('joi');
+app.use(logger);
+  
+
+// Error Hanlder Middleware
+app.use(notFound);
+app.use(errorHanlder);
+  
+  
 
 
 app.use("/api/books" , bookspath)
 app.use("/api/authors" , authorspath)
 
 
-
-app.listen(5000, () => {
-  console.log('server is listening on port 5000...');
-});
+const PORT = process.env.PORT || 5000 ;
+app.listen(PORT, () => console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
---
+++ models/User.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose")
+const Joi = require('joi');
+
+
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength:5,
+    maxlength:100,
+    unique:true,
+},
+userName: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength:2,
+  maxlength:200,
+},
+
+password: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength:6,
+},
+
+isAdmin: {
+  type: Joi.boolean,
+  default: "false",
+},
+},{
+  timestamps: true
+})
+
+
+const User = mongoose.model("User", UserSchema);
+
+//validate register functions
+function validateRegisterUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).required().email(),
+    userName: Joi.string().trim().min(2).max(200).required(),
+    password: Joi.string().trim().min(6).required(),
+    isAdmin: Joi.bool(),
+})
+
+  return schema.validate(obj) ;
+}
+
+
+
+//validate login functions
+function validateLoginUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).required().email(),
+    password: Joi.string().trim().min(6).required(),
+})
+
+  return schema.validate(obj) ;
+}
+
+
+
+//validate update functions
+function validateUpdateUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(5).max(100).email(),
+    userName: Joi.string().trim().min(2).max(200),
+    password: Joi.string().trim().min(6),
+    isAdmin: Joi.bool(),
+})
+
+  return schema.validate(obj) ;
+}
+
+
+
+
+
+module.exports = {
+  User,
+  validateRegisterUser,
+  validateLoginUser,
+  validateUpdateUser,
+}
